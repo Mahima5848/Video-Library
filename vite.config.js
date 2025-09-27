f2700dfa4fef72494cde463f94451a0ab13463a2
Mahima Kushwaha <mahimakushwaha098@gmail.com>
2025-09-27 16:57:29 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Video-Library/',  // aapke repo ka naam
+  base: '/Video-Library/',   // <-- Repo ka naam
 })
